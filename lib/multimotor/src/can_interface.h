9fb0dfe8ed35a903cf2f65a0596781a9ff2fd041
Tim O'Brien <timo@t413.com>
2025-07-22 17:30:15 -0700

    📦 move multimotor library to submodule

--- lib/multimotor/src/can_interface.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-
-class CanInterface {
-public:
-    virtual void send(uint32_t id, uint8_t* data, uint8_t len, bool extended, bool ss = true, bool rtr = false) = 0;
-};
-
