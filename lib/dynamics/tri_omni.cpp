22d82cc0d08b9ce6deb3fd2d9e91ad90a4b2f34e
Tim O'Brien <timo@t413.com>
2026-07-21 18:00:32 -0700

    rover: balance gains, mode switching fixes/cleanup
    - seems that CyberGear never reports it's in current mode :/

--- lib/dynamics/tri_omni.cpp
+++ lib/dynamics/tri_omni.cpp
@@ -22,9 +22,12 @@ void TriOmni::init() {
   ctx_->addAdjustable(&balanceSpeedCtrl_.I, "spd.I");
   ctx_->addAdjustable(&balanceSpeedCtrl_.D, "spd.D");
   ctx_->addAdjustable(&balanceSpeedCtrl_.rampLimit, "spd.Rl");
+  ctx_->addAdjustable(&balanceSpeedCtrl_.limit, "spd.lm");
   ctx_->addAdjustable(&balanceYawCtrl_.P, "b.yaw.P");
   ctx_->addAdjustable(&balanceYawCtrl_.I, "b.yaw.I");
   ctx_->addAdjustable(&balanceYawCtrl_.D, "b.yaw.D");
+  ctx_->addAdjustable(&balanceYawCtrl_.rampLimit, "b.yaw.Rl");
+  ctx_->addAdjustable(&balanceYawCtrl_.limit, "b.yaw.lm");
 
   auto drives = ctx_->getDrives();
   auto dcount = ctx_->getDriveCount(false);
@@ -86,60 +89,61 @@ void TriOmni::iterate(uint32_t now, const SharedState& state, DriveManager& dm) 
     D_LOG("Balancing mode %s", isBalancing_? "enabled" : "disabled");
     resetPids();
     lastBalanceChange_ = now;
+    for (uint8_t d = 0; d < dcount; d++) {
+      if (!drives[d]) continue;
+      auto wantmode = enabled? ((isBalancing_ && d != BACK)? MotorMode::Current : MotorMode::Speed) : MotorMode::Disabled;
+      drives[d]->setMode(wantmode);
+      D_LOG("m[%d] set mode %d", d, wantmode);
+    }
   }
 
   //main control loop
-  if (dcount) { //at least one
-    m.fwd  *= m.maxSpeed;
-    m.side *= m.maxSpeed;
-    m.yaw  *= m.maxSpeed;
+  if (dcount != 3) {
+    D_LOG("strange drive number %d", dcount);
+    return;
+  }
+  m.fwd  *= m.maxSpeed;
+  m.side *= m.maxSpeed;
+  m.yaw  *= m.maxSpeed;
 
-    m.fwd  += state.activeCmd.d_fwd;
-    m.side += state.activeCmd.d_side;
-    m.yaw  += state.activeCmd.d_yaw;
+  m.fwd  += state.activeCmd.d_fwd;
+  m.side += state.activeCmd.d_side;
+  m.yaw  += state.activeCmd.d_yaw;
 
-    float y = -m.yaw; //convert yaw to angular rate
+  float y = -m.yaw; //convert yaw to angular rate
 
-    if (lastBalanceChange_ == now) {
-      for (uint8_t d = 0; d < dcount; d++)
-        if (d != BACK && drives[d]) //back stays in speed mode
-          drives[d]->setMode(isBalancing_? MotorMode::Current : MotorMode::Speed);
+  float vfwd = 0, vyaw = 0;
+  calcBalanceSpeeds(state, vfwd, vyaw);
+  if (isBalancing_) {
+
+    // Blend between angle-control and odometry speed control
+    const uint32_t balanceChangeDuration = 2000; //ms
+    float pitchGoal = balanceModeSpeed? balanceSpeedCtrl_.update(now, vfwd - m.fwd) : -m.fwd;
+    pitchGoal += state.activeCmd.pitchOffsetDeg;
+    if (balanceModeSpeed && (now - lastBalanceChange_) < balanceChangeDuration) {
+      const float blendT = (now - lastBalanceChange_) / (float)balanceChangeDuration;
+      pitchGoal = blend( -m.fwd, pitchGoal, blendT);
+      if (blendT < 0.5f)
+        balanceSpeedCtrl_.reset(); //prevent rampup while not in control
     }
+    float torqueCmd = balanceCtrl_.update(now, pitchGoal - pitchFwd); //input is angle
+    torqueCmd *= 10.0; //roughly scale to Nm
+    m.fwd = torqueCmd; // Use torque output directly
 
-    float vfwd = 0, vyaw = 0;
-    calcBalanceSpeeds(state, vfwd, vyaw);
-    if (isBalancing_) {
-
-      // Blend between angle-control and odometry speed control
-      const uint32_t balanceChangeDuration = 2000; //ms
-      float pitchGoal = balanceModeSpeed? balanceSpeedCtrl_.update(now, vfwd - m.fwd) : -m.fwd;
-      pitchGoal += state.activeCmd.pitchOffsetDeg;
-      if (balanceModeSpeed && (now - lastBalanceChange_) < balanceChangeDuration) {
-        const float blendT = (now - lastBalanceChange_) / (float)balanceChangeDuration;
-        pitchGoal = blend( -m.fwd, pitchGoal, blendT);
-        if (blendT < 0.5f)
-          balanceSpeedCtrl_.reset(); //prevent rampup while not in control
-      }
-      float torqueCmd = balanceCtrl_.update(now, pitchGoal - pitchFwd); //input is angle
-      torqueCmd *= 10.0; //roughly scale to Nm
-      m.fwd = torqueCmd; // Use torque output directly
-
-      y = balanceYawCtrl_.update(now, (y + -m.side) - vyaw); //input is speed, output is torque
-      m.side = 0; //disable side
-      D_LOG("(fwd %06.2f vfwd%06.2f)-> [-pgoal %06.2f -p %06.2f] vyaw(%06.2f) -> %06.2f ch6(%d)",
-        m.fwd, vfwd, pitchGoal, pitchFwd, vyaw, y, state.crsfChans[6]);
-    } else {
-      balanceSpeedCtrl_.reset();
-      balanceCtrl_.reset();
-    }
-    if (enabled) {
-      //TODO check each drive pointer
-      drives[BACK]->setSetpoint(MotorMode::Speed, isBalancing_? vyaw : (y  +   0   + m.side));
-      drives[LEFT]->setSetpoint(isBalancing_? MotorMode::Current : MotorMode::Speed, y  - m.fwd   - m.side * 1.33/2);
-      drives[RGHT]->setSetpoint(isBalancing_? MotorMode::Current : MotorMode::Speed, y  + m.fwd   - m.side * 1.33/2);
-    }
-
-  } //drive count check
+    y = balanceYawCtrl_.update(now, (y + -m.side) - vyaw); //input is speed, output is torque
+    m.side = 0; //disable side
+    D_LOG("(fwd %06.2f vfwd%06.2f)-> [-pgoal %06.2f -p %06.2f] vyaw(%06.2f) -> %06.2f m[%d,%d,%d]",
+      m.fwd, vfwd, pitchGoal, pitchFwd, vyaw, y, state.motorStates[0].mode, state.motorStates[1].mode, state.motorStates[2].mode);
+  } else {
+    balanceSpeedCtrl_.reset();
+    balanceCtrl_.reset();
+  }
+  if (enabled) {
+    //TODO check each drive pointer
+    drives[BACK]->setSetpoint(MotorMode::Speed, isBalancing_? vyaw : (y  +   0   + m.side));
+    drives[LEFT]->setSetpoint(isBalancing_? MotorMode::Current : MotorMode::Speed, y  - m.fwd   - m.side * 1.33/2);
+    drives[RGHT]->setSetpoint(isBalancing_? MotorMode::Current : MotorMode::Speed, y  + m.fwd   - m.side * 1.33/2);
+  }
 
   status_ = isBalancing_? "woah." : enabled? "wee!" : ":|";
 }
--- lib/dynamics/tri_omni.h
+++ lib/dynamics/tri_omni.h
@@ -6,7 +6,7 @@
 
 class TriOmni : public DynamicsBase {
   PIDCtrl balanceCtrl_ = PIDCtrl(0.6, 0.0, 0.1, 2); //inputs angle -> outputs torque (in A)
-  PIDCtrl balanceSpeedCtrl_ = PIDCtrl(90.0, 0.0, 0.4, 30, 60); //inputs speed -> outputs angle
+  PIDCtrl balanceSpeedCtrl_ = PIDCtrl(90.0, 0.0, 0.4, 15, 100); //inputs speed -> outputs angle
   PIDCtrl balanceYawCtrl_ = PIDCtrl(13.0, 0.0, 0.1, 2, 100); //outputs torque
   uint32_t lastBalanceChange_ = 0;
   bool yawCtrlEnabled_ = false;
